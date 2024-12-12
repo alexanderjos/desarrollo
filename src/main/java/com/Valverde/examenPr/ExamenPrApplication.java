bb3dbb358ae6b03092718fe2e813a2c4005fbafe
Alexander Valverde <94985312+alexanderjos@users.noreply.github.com>
2024-12-12 00:30:14 -0500

    Add files via upload

---
+++ src/main/java/com/Valverde/examenPr/ExamenPrApplication.java
@@ -0,0 +1,13 @@
+package com.Valverde.examenPr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Valverde/examenPr/ExamenPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Valverde.examenPr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
